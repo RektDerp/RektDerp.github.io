29f3de89a4ffc772b2e0d31cb829f46c8eee11f0
Derpy Hooves <rdmail228@gmail.com>
2021-10-26 14:19:05 +0300

    modified:   src/img/online_background_0cd9a99a9a16530cceca7f28bed954c8.jpg 	modified:   src/index.html 	modified:   src/script/index.js 	modified:   src/styles/index.css

--- src/script/index.js
+++ src/script/index.js
@@ -1,14 +1,48 @@
-document.getElementById("butt").addEventListener("click", function () {
-  alert("sas");
+parent = document.getElementById('parent');
+
+workaround = class{
+  elem;
+  constructor(e){
+    this.elem = e;
+  }
+  handleEvent(){
+    this.elem.remove();
+    console.log('destroyed');
+  }
+}
+
+document.getElementById("butt").addEventListener("click",() => {
+  spawnTriangle();
 });
 
+function spawnTriangle(){
+  size = 2 + Math.random() * 15;
+  light = 40 + Math.random() * 55;
+  xpos = Math.random() * 28;
+  dur = 4 + Math.random() * 5;
 
+  newDiv = document.createElement("div");
+  newDiv.classList.add('triangle');
+  newDiv.style.setProperty('--base', size + 'vw');
+  newDiv.style.setProperty('--light', light + '%');
+  newDiv.style.setProperty('--xpos', xpos + 'vw');
+  newDiv.style.setProperty('--speed', dur + 's');
+  console.log('size ' + size + ', light ' + light + ', xpos ' + xpos);
+  document.getElementById('parent').appendChild(newDiv);
+  crutch = new workaround(newDiv);
+  newDiv.addEventListener('animationend', crutch);
 
+  return newDiv;
+}
 
+//spawnTriangle();
 
-
-
-
+/*
+for(let i = 0; i < 100; i++){
+  newDiv = document.createElement("div");
+  newDiv.classList.add('triangle');
+  document.getElementById('parent').appendChild(newDiv);
+}
 
 
 let elements = document.getElementsByClassName('triangle');
@@ -17,14 +51,22 @@ var size = 20;
 var light;
 var xpos;
 var dur;
+
+
 for(let i = 0; i < elements.length; i++){
   size = size - Math.random() * 5;
   light = 40 + Math.random() * 55;
   xpos = Math.random() * 28;
-  dur = 3 + Math.random() * 6;
-  console.log(i + '. size ' + size + ', light ' + light + ', xpos ' + xpos);
+  dur = 5 + Math.random() * 4;
   elements[i].style.setProperty('--base', size + 'vw');
   elements[i].style.setProperty('--light', light + '%');
   elements[i].style.setProperty('--xpos', xpos + 'vw');
   elements[i].style.setProperty('--speed', dur + 's');
+  console.log(i + '. size ' + size + ', light ' + light + ', xpos ' + xpos);
+
+  var delegate = function(element){
+    element.remove();
+  }
+  setTimeout(delegate, (dur) * 1000, elements[i]);
 };
+*/
