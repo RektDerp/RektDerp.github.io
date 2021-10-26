164e3be8d58bd7a332495dea61c989236d7430e7
Derpy Hooves <rdmail228@gmail.com>
2021-10-26 14:21:04 +0300

    commit

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-parent = document.getElementById('parent');
 
 workaround = class{
   elem;
@@ -34,39 +33,3 @@ function spawnTriangle(){
 
   return newDiv;
 }
-
-//spawnTriangle();
-
-/*
-for(let i = 0; i < 100; i++){
-  newDiv = document.createElement("div");
-  newDiv.classList.add('triangle');
-  document.getElementById('parent').appendChild(newDiv);
-}
-
-
-let elements = document.getElementsByClassName('triangle');
-
-var size = 20;
-var light;
-var xpos;
-var dur;
-
-
-for(let i = 0; i < elements.length; i++){
-  size = size - Math.random() * 5;
-  light = 40 + Math.random() * 55;
-  xpos = Math.random() * 28;
-  dur = 5 + Math.random() * 4;
-  elements[i].style.setProperty('--base', size + 'vw');
-  elements[i].style.setProperty('--light', light + '%');
-  elements[i].style.setProperty('--xpos', xpos + 'vw');
-  elements[i].style.setProperty('--speed', dur + 's');
-  console.log(i + '. size ' + size + ', light ' + light + ', xpos ' + xpos);
-
-  var delegate = function(element){
-    element.remove();
-  }
-  setTimeout(delegate, (dur) * 1000, elements[i]);
-};
-*/
